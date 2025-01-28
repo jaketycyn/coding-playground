1a4511b88b0876b3ffc9596dea593f77080ebc94
Jake TyCyn <jaketycyn@gmail.com>
2025-01-28 14:03:50 -0800

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>New item link for next thing</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
