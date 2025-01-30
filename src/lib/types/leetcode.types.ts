dc3ab7d0037df14bfc8f36f58005ab47673b568c
Jake TyCyn <jaketycyn@gmail.com>
2025-01-29 19:21:40 -0800

    chore(types) - types for Solution

--- src/lib/types/leetcode.types.ts
+++ src/lib/types/leetcode.types.ts
@@ -1,13 +1,19 @@
+export interface Solution {
+  code: string;
+  approach: string;
+  description: string;
+  timeComplexity: string;
+  spaceComplexity: string;
+}
+
 export interface LeetCodeProblem {
   id: string;
   title: string;
   difficulty: "Easy" | "Medium" | "Hard";
   problemNumber: number;
-  categories: string[];
-  solution: {
-    typescript?: string;
-    python?: string;
+  categories: string[]; // could be made into its own type later
+  solutions: {
+    typescript?: Solution[];
+    python: Solution[];
   };
-  timeComplexity: string;
-  spaceComplexity: string;
 }
