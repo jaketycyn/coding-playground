fb22585bf9bafda11364b4ba82ce37543fc6c526
Jake TyCyn <jaketycyn@gmail.com>
2025-02-18 23:36:49 -0800

    docs(content) - LC 49 Typescript Solution

--- content/leetcode/49-group-anagrams/typescript/solution-1.ts
+++ content/leetcode/49-group-anagrams/typescript/solution-1.ts
@@ -1,11 +1,25 @@
 /**
- * Problem: [Problem Name]
+ * 49: Group Anagrams
  * @approach approachDescription
  * @description moreDetailedDescription
  * @timeComplexity O(n²) | O(log₂n) | O(logₙn) | O(nlogn) | O(1)
  * @spaceComplexity O(n²) | O(log₂n) | O(logₙn) | O(nlogn) | O(1)
  */
 
-export function solution() {
-  // Implementation here
+export function groupAnagrams(strs: string[]): string[][] {
+  // map for storing strs
+  let anagramMap = new Map<string, string[]>();
+
+  // iterate
+  strs.forEach((str) => {
+    // sortedStr creation from str
+    let sortedStr = [...str].sort().join("");
+
+    // find group
+    let anagramGroup = anagramMap.get(sortedStr) ?? [];
+
+    anagramMap.set(sortedStr, [...anagramGroup, str]);
+  });
+
+  return [...anagramMap.values()];
 }
