9c4f57b683b619053ddc27b81d080a2c67691a7d
Jake TyCyn <jaketycyn@gmail.com>
2025-03-05 23:02:56 -0800

    refactor(badges) - Update to Badges info

--- src/app/_components/shared/Badges/index.tsx
+++ src/app/_components/shared/Badges/index.tsx
@@ -6,16 +6,16 @@ export const Badge = ({
   children,
 }: BadgesProps) => {
   const variantClasses = {
-    default: "bg-gray-100 text-gray-800",
-    success: "bg-green-100 text-green-800",
-    warning: "bg-yellow-100 text-yellow-800",
-    error: "bg-red-100 text-red-800",
-    outline: "border border-gray-200 text-gray-600",
+    default: "bg-gray-100 text-black hover:bg-gray-200",
+    success: "bg-green-600 text-black hover:bg-green-700",
+    warning: "bg-yellow-400 text-black hover:bg-yellow-500",
+    error: "bg-red-500 text-black hover:bg-red-600",
+    outline: "border border-gray-200 text-gray-600 hover:bg-slate-800",
   };
 
   return (
     <span
-      className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${variantClasses[variant]} ${className}`}
+      className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium  ${variantClasses[variant]} ${className}`}
     >
       {children}
     </span>
